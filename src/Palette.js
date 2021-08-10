a4d2a8b02009a01f3f59d525c203ded4f023d8eb
Pablo del Barrio <pablodbb@gmail.com>
2021-08-10 21:08:09 +0200

    Created react-app, added palette component

---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/*Aquí va la navbar */}
+        <div className="Palette-colors">{/* Un puñao de color boxes */}</div>
+        {/*Footer aqui*/}
+      </div>
+    );
+  }
+}
+export default Palette;
