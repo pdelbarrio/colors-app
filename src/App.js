6c1eac0839b38fece4ce24eec6a59d1a523db22c
Pablo del Barrio <pablodbb@gmail.com>
2021-08-13 21:54:06 +0200

    Add Link to ColorBox and Add Route

--- src/App.js
+++ src/App.js
@@ -32,6 +32,10 @@ class App extends Component {
             />
           )}
         />
+        <Route
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>SINGLE COLOR PAGE</h1>}
+        />
       </Switch>
 
       //<div>
